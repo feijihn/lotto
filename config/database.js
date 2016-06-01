5877c3c653b5bc3e4bbafabdc68f5f2bed2bd3ee
Architektor <gloosx@gmail.com>
2016-06-01 22:10:21 +0300

    added mongo URI

--- config/database.js
+++ config/database.js
@@ -1,4 +1,10 @@
-// config/database.js
-module.exports = {
-  url: 'mongodb://localhost/lotalot' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-};
+// config/database.json
+if (process.env.NODE_ENV === 'production') {
+  module.exports = {
+    url: 'mongodb://heroku_mlv22k2d:8bji0m402c0h6vl4f79i0nq2m1@ds019123.mlab.com:19123/heroku_mlv22k2d' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+  };
+} else {
+  module.exports = {
+    url: 'mongodb://localhost/lotalot' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+  };
+}
