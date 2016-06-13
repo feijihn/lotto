561887a0abf552504420a7f74dad19eb6d65cafa
Architektor <gloosx@gmail.com>
2016-06-13 15:48:43 +0300

    advanced error handling!

--- server/roundLogic.js
+++ server/roundLogic.js
@@ -103,14 +103,14 @@ var chooseWinner = function(date, isCheck) {
                     }
                   }
                 });
-              } catch (e) {
-                console.log('[BLOCKCHAIN] Something went wrong when working with RAW block....\n  ERR: ' + e + '\nStack:' + e.stack);
+              } catch (error) {
+                console.err('When working with RAW block occured -- ', error);
                 clearInterval(handle);
               }
             });
           }
-        } catch (err) {
-          console.log('[BLOCKCHAIN] Something went wrong when working with blocks....\n  ERR: ' + err + '\nStack:' + err.stack);
+        } catch (error) {
+          console.err('When working with block occured -- ', error);
           clearInterval(handle);
         }
       });
@@ -119,53 +119,76 @@ var chooseWinner = function(date, isCheck) {
 };
 
 function generateWinnerMessage(rndId) {
-  return 'Greetings!\n Your ticket was the winner\'s one in the round id ' + rndId + '! Please contact round maintainer to claim your prize.';
+  try {
+    let message = 'Greetings!\n Your ticket was the winner\'s one in the round id ' + rndId + '! Please contact round maintainer to claim your prize.';
+    return message;
+  } catch (error) {
+    console.err('When generating winenr message occured -- ', error)
+  }
 }
 
 function generateRoundEndMessage(rndId) {
-  return 'Hello.\nRound with id ' + rndId + ' y in has ended. You are not the winner today, better luck next time!';
+  try {
+    let message = 'Hello.\nRound with id ' + rndId + ' y in has ended. You are not the winner today, better luck next time!';
+    return message;
+  } catch(error) {
+    console.err('When generating roudn end message occured -- ', error);
+  }
 }
 
 function addParticipant(rndId, userId) {
   Round.findOneAndUpdate({_id: rndId, participants: {$nin: [String(userId)]}, endTime: undefined}, {$push: {participants: String(userId)}}, (err, round) => {
-    if (err) {
-      throw err;
+    try {
+      if (err) {
+        throw err;
+      }
+    } catch(error) {
+      console.err('When adding participant to round occured --', error);
     }
   });
 }
 
 function checkRoundEnd(rndId) {
   Round.findOne({_id: rndId, endTime: {$eq: undefined}}, (err, round) => {
-    if (err) {
-      throw err;
-    }
-    if (round) {
-      console.log('checking if round ' + round._id + ' ended...');
-      console.log('round has ' + round.tickets.length + ' tickets');
-      if (round.tickets.length >= 100) {
-        let end = Date.now();
-        Round.findByIdAndUpdate(rndId, {$set: {endTime: end}}, err => {
-          if (err) {
-            throw err;
-          }
-          chooseWinner(end).then(
-            result => {
-              let winnum = result[0];
-              let handle = result[1];
-              clearInterval(handle);
-              console.log('winner is ticket number ' + winnum);
-              Round.findByIdAndUpdate(rndId, {$set: {winnum: winnum}}, (err, round) => {
-                if (err) {
-                  throw err;
-                }
-                sendAlertsToParticipants(rndId, winnum);
-                handleNextRound(round.product_id);
-              });
-            },
-            error => console.log('winner not selected!')
-          );
-        });
+    try {
+      if (err) {
+        throw err;
       }
+      if (round) {
+        console.server('Checking if round ' + round._id + ' ended...');
+        console.server('Round has ' + round.tickets.length + ' tickets');
+        if (round.tickets.length >= 100) {
+          console.server('Round ' + round._id + ' ended. Will choose winner now...');
+          let end = Date.now();
+          Round.findByIdAndUpdate(rndId, {$set: {endTime: end}}, err => {
+            try {
+              if (err) {
+                throw err;
+              }
+              chooseWinner(end).then(
+                result => {
+                  let winnum = result[0];
+                  let handle = result[1];
+                  clearInterval(handle);
+                  console.server('Winner is ticket number ' + winnum);
+                  Round.findByIdAndUpdate(rndId, {$set: {winnum: winnum}}, (err, round) => {
+                    if (err) {
+                      throw err;
+                    }
+                    sendAlertsToParticipants(rndId, winnum);
+                    handleNextRound(round.product_id);
+                  });
+                },
+                error => console.err('When choosing winner occured --', error)
+              );
+            } catch(error) {
+              console.err('When updating round occured -- ', error);
+            }
+          });
+        }
+      }
+    } catch(error) {
+      console.err('When checking round end occured -- ', error);
     }
   });
 }
@@ -177,54 +200,73 @@ function createNewTicket(rndId, userId, value) {
   newTicket.user_id = userId;
   newTicket.value = value;
   newTicket.save(err => {
-    if (err) {
-      throw err;
-    }
-    Round.findByIdAndUpdate(rndId, {$push: {tickets: newTicket}}, (err, round) => {
+    try {
       if (err) {
         throw err;
       }
-    });
+      Round.findByIdAndUpdate(rndId, {$push: {tickets: newTicket}}, (err, round) => {
+        try {
+          if (err) {
+            throw err;
+          }
+        } catch(error) {
+          console.err('When updating round occured -- ', error);
+        }
+      });
+    } catch(error) {
+      console.err('When creating new ticket occured -- ', error);
+    }
   });
   return newTicket;
 }
 
 function createNewRound(prodId, running) {
-  let newRound = new Round();
-  console.log('creating new round...');
-  newRound.product_id = prodId;
-  newRound.running = running;
-  if (running) {
-    newRound.startTime = Date.now();
-  }
-  newRound.creationTime = Date.now();
-  newRound.save((err, round) => {
-    if (err) {
-      throw err;
+  try {
+    console.server('Creating new round...');
+    newRound.product_id = prodId;
+    newRound.running = running;
+    if (running) {
+      newRound.startTime = Date.now();
     }
-  });
-  return newRound._id;
-}
-
-function handleNextRound(prodId) {
-  console.log('running closest round...');
-  Round.find({product_id: prodId, startTime: undefined}, (err, rounds) => {
-    if (err) {
-      throw err;
-    }
-    if (rounds.length) {
-      let creationTimes = rounds.map(round => {
-        return round.creationTime;
-      });
-      let earliest = Math.min(...creationTimes);
-      Round.findOneAndUpdate({creationTime: earliest}, {$set: {running: true, startTime: Date.now()}}, err => {
+    newRound.creationTime = Date.now();
+    newRound.save((err, round) => {
+      try {
         if (err) {
           throw err;
         }
-      });
-    } else {
-      console.log('no more rounds exist... creating new...');
-      createNewRound(prodId, true);
+      } catch(error) {
+        console.err("When saving new round occured -- ", error);
+      }
+    });
+    return newRound._id;
+  } catch(error) {
+    console.err('When creating new round occured -- ', error);
+  }
+}
+
+function handleNextRound(prodId) {
+  console.server('Running closest round...');
+  Round.find({product_id: prodId, startTime: undefined}, (err, rounds) => {
+    try {
+      if (err) {
+        throw err;
+      }
+      if (rounds.length) {
+        let creationTimes = rounds.map(round => {
+          return round.creationTime;
+        });
+        let earliest = Math.min(...creationTimes);
+        Round.findOneAndUpdate({creationTime: earliest}, {$set: {running: true, startTime: Date.now()}}, err => {
+          if (err) {
+            throw err;
+          }
+        });
+      } else {
+        console.server('No more rounds exist... creating new...');
+        createNewRound(prodId, true);
+      }
+    } catch(error) {
+      console.err('When handling next round occured --', error)
     }
   });
 }
@@ -273,57 +315,65 @@ function sendAlertsToParticipants(rndId, winner) {
 module.exports.ownTicket = function(rndId, userId, value) {
   let prodId;
   Round.findOne({_id: rndId}, (err, round) => {
-    if (err) {
-      throw err;
+    try {
+      if (err) {
+        throw err;
+      }
+      prodId = round.product_id;
+    } catch(error) {
+      console.err('Can`t find round (id ' + rndId + ') make sure it exists.', error);
     }
-    prodId = round.product_id;
   });
   Ticket.find({round_id: rndId, value: value}, (err, ticket) => {
-    if (err) {
-      throw err;
-    }
-    if (ticket.length === 0) {
-      createNewTicket(rndId, userId, value);
-    } else {
-      console.log('ticket collision... resolving...');
-      setTimeout(() => {
-        Round.find({product_id: prodId, startTime: undefined}, (err, rounds) => {
-          if (err) {
-            throw err;
-          }
-          if (rounds.length) {
-            let creationTimes = rounds.map(round => {
-              return round.creationTime;
-            });
-            for (var i = 0; i < creationTimes.length; ++i) {
-              let break_ = false;
-              Round.findOne({creationTime: creationTimes[i]}, (err, round) => {
-                if (err) {
-                  throw err;
-                }
-                Ticket.findOne({round_id: round._id, value: value}, (err, ticket) => {
-                  console.log('round with id ' + round._id + ' contains ' + ticket);
+    try {
+      if (err) {
+        throw err;
+      }
+      if (ticket.length === 0) {
+        createNewTicket(rndId, userId, value);
+      } else {
+        console.log('ticket collision... resolving...');
+        setTimeout(() => {
+          Round.find({product_id: prodId, startTime: undefined}, (err, rounds) => {
+            if (err) {
+              throw err;
+            }
+            if (rounds.length) {
+              let creationTimes = rounds.map(round => {
+                return round.creationTime;
+              });
+              for (var i = 0; i < creationTimes.length; ++i) {
+                let break_ = false;
+                Round.findOne({creationTime: creationTimes[i]}, (err, round) => {
                   if (err) {
                     throw err;
                   }
-                  if (!ticket) {
-                    createNewTicket(round._id, userId, value);
-                    console.log('added conflicting ticket w/ value ' + value + ' to round id ' + round._id);
-                    break_ = true;
-                  }
+                  Ticket.findOne({round_id: round._id, value: value}, (err, ticket) => {
+                    console.log('round with id ' + round._id + ' contains ' + ticket);
+                    if (err) {
+                      throw err;
+                    }
+                    if (!ticket) {
+                      createNewTicket(round._id, userId, value);
+                      console.log('added conflicting ticket w/ value ' + value + ' to round id ' + round._id);
+                      break_ = true;
+                    }
+                  });
                 });
-              });
-              if (break_) {
-                break;
+                if (break_) {
+                  break;
+                }
               }
+            } else {
+              console.log('no more rounds exist... creating new...');
+              let freshRound = createNewRound(prodId, false);
+              createNewTicket(freshRound, userId, value);
             }
-          } else {
-            console.log('no more rounds exist... creating new...');
-            let freshRound = createNewRound(prodId, false);
-            createNewTicket(freshRound, userId, value);
-          }
-        });
-      }, 1000);
+          });
+        }, 1000);
+      }
+    } catch(error) {
+      console.err('When trying to find tickets occured -- ', error)
     }
   });
 };
