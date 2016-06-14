b791e4c645eb8c7c9ce8e79d1ef79de03477cc33
Architektor <gloosx@gmail.com>
2016-06-14 22:20:30 +0300

    fixed admin index page

--- admin-app/components/Index.jsx
+++ admin-app/components/Index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Index extends React.Component {
   render() {
     return (
-      <div className={'panel'}>
+      <div className={'admin-panel__content'}>
          <h1>This is Admin Panel</h1>
       </div>
     );
