a598c0130426a5c584407036bef90ee359bb29bb
Architektor <gloosx@gmail.com>
2016-06-14 22:20:10 +0300

    styled productsa bit

--- app/components/client/Index/Sections/Products/Product/component.jsx
+++ app/components/client/Index/Sections/Products/Product/component.jsx
@@ -7,7 +7,7 @@ export default class Product extends React.Component {
         <div className={'product col-lg-3 col-md-3 col-sm-4 col-xs-12'}>
           {this.props.children}
           <Link to={'/round/' + this.props.id}>
-            <button type="button" className={'product__button'}>Выйграть</button>
+            <button type="button" className={'product__button'}>Выиграть</button>
           </Link>
         </div>
     );
