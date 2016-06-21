1e8bd0cb2ab1413cd64cbb3bfb0a73a5505d652e
Architektor <gloosx@gmail.com>
2016-06-21 13:51:13 +0300

    robust content submitting done.

--- server/routes/admin/pagesCRUD.js
+++ server/routes/admin/pagesCRUD.js
@@ -8,33 +8,48 @@ const access = require('../../middlewares/access.js');
 const isLoggedIn = access.isLoggedIn;
 const isAdmin = access.isAdmin;
 
+function submitContents(contents) {
+  return new Promise((resolve, reject) => {
+    contents.forEach((content, i) => {
+      let newContent = new Content();
+      newContent.name = content.name;
+      newContent.header = content.header;
+      newContent.text = content.text.replace(/\r?\n/g, '<br />');
+      Content.findOne({name: content.name}, (err, _content) => {
+        if (err) {
+          reject(err);
+        } 
+        if (_content) {
+          Content.remove({_id: _content._id}, (err, query) => {
+            if (err) {
+              reject(err);
+            }
+            newContent.save(err => {
+              if (err) {
+                reject(err);
+              }
+              if (i === contents.length - 1) {
+                resolve('done');
+              }
+            });
+          });
+        }
+      });
+    });
+  });
+}
+
 module.exports = app => {
   app.post('/submitcontent', isLoggedIn, isAdmin, (req, res) => {
-      let newContent = new Content();
-      newContent.name = 'introSection';
-      newContent.header = req.body.introHeader;
-      let text = req.body.introText.replace(/\r?\n/g, '<br />');
-      newContent.text = text;
-      let newContent2 = new Content();
-      newContent2.name = 'reliabilitySection';
-      newContent2.header = req.body.reliabilityHeader;
-      text = req.body.reliabilityText.replace(/\r?\n/g, '<br />');
-      newContent2.text = text;
-      Content.remove({}, err => {
-        if (err) {
-          throw err;
-        }
-        newContent.save(err => {
-          if (err) {
-            throw err;
-          }
-        });
-        newContent2.save(err => {
-          if (err) {
-            throw err;
-          }
-        });
-      });
-    res.sendStatus(200);
+    let contents = []
+    Object.keys(req.body).forEach(key => {
+      req.body[key].name = key;
+      contents.push(req.body[key]);
+    });
+    submitContents(contents)
+      .then(
+        result => res.send(result),
+        error => console.err(error)
+      )
   });
 }
